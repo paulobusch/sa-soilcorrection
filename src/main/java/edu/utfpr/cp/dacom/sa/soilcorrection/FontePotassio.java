3c2923dbb944a92edc8ae8337eb9414ec8a8ac37
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-07-29 20:23:58 -0300

    Refactore FontePotassio

--- src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/FontePotassio.java
+++ src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/FontePotassio.java
@@ -1,30 +1,19 @@
 package edu.utfpr.cp.dacom.sa.soilcorrection;
 
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import java.util.Set;
 
+@RequiredArgsConstructor
 public enum FontePotassio implements IFonteNutriente {
     CLORETO_POTASSIO(0.58, Set.of()),
     SULFATO_POTASSIO(0.52, Set.of(new NutrienteAdicional(NomeNutrienteAdicional.ENXOFRE, 0.17))),
     SULFATO_POTASSIO_MAGNESIO(0.22, Set.of(new NutrienteAdicional(NomeNutrienteAdicional.ENXOFRE, 0.22), new NutrienteAdicional(NomeNutrienteAdicional.MAGNESIO, 0.18)));
-    
+
+    @Getter @NonNull
     private final double teorFonte;
+
+    @Getter @NonNull
     private final Set<NutrienteAdicional> nutrientesAdicionais;
-
-    FontePotassio(
-        double teorFonte,
-        final Set<NutrienteAdicional> nutrientesAdicionais) {
-
-        this.teorFonte = teorFonte;
-        this.nutrientesAdicionais = nutrientesAdicionais;
-    }
-
-    @Override
-    public double getTeorFonte() {
-        return this.teorFonte;
-    }
-
-    @Override
-    public Set<NutrienteAdicional> getNutrientesAdicionais() {
-        return this.nutrientesAdicionais;
-    }
 }
